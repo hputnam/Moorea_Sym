61a43903e985fbe9c828eb11f12ce2a462c1b5b2
Hollie Putnam <hollieputnam@gmail.com>
2017-05-19 15:43:48 -0400

    added unifrac and trees

--- RAnalysis/Scripts/tree_building.R
+++ RAnalysis/Scripts/tree_building.R
@@ -1,16 +1,18 @@
 library(seqinr)
+library(phangorn) 
+library(caroline)
 
 #https://rdrr.io/rforge/seqinr/man/dist.alignment.html
 #returns sqrt of pairwise genetic distance, then squared the matrices
-A.seqs <- read.alignment(file = "Data/A_tree_seqs_aligned.fasta", format= "fasta")
+A.seqs <- read.alignment(file = "Data/A_tree_seqs_aligned_clean.fasta", format= "fasta")
 A.dis <- (as.matrix(dist.alignment(A.seqs, matrix = "identity" )))^2
 write.csv(A.dis, file="Data/A.dis.matx.csv")
 
-C.seqs <- read.alignment(file = "Data/C_tree_seqs_aligned.fasta", format= "fasta")
+C.seqs <- read.alignment(file = "Data/C_tree_seqs_aligned_clean.fasta", format= "fasta")
 C.dis <- (as.matrix(dist.alignment(C.seqs, matrix = "identity" )))^2
 write.csv(C.dis2, file="Data/C.dis.matx.csv")
 
-D.seqs <- read.alignment(file = "Data/D_tree_seqs_aligned.fasta", format= "fasta")
+D.seqs <- read.alignment(file = "Data/D_tree_seqs_aligned_clean.fasta", format= "fasta")
 D.dis <- (as.matrix(dist.alignment(D.seqs, matrix = "identity" )))^2
 write.csv(D.dis, file="D.dis.matx.csv")
 
@@ -27,9 +29,17 @@ ubermatrix <- cbind(col1, col2, col3)
 dim(ubermatrix)
 
 #build tree
+uber.tree <- upgma(ubermatrix)
+plot(uber.tree, main="UPGMA")
+
+#write tree to file
+write.tree(uber.tree, file="uber.tre")
 
 #use tree and OTU table for calculating beta_diversity.py
 #http://qiime.org/scripts/beta_diversity.html
-  
+
+write.delim(data.frame(otu_table(phy.f)), "otu_table.tsv", quote = FALSE, row.names = T, sep = "\t")
+
+
   
   
